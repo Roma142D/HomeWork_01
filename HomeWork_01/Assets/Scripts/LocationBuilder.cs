2150b8c97eec85a728353098b793fd6e0f451fa3
Roman Doliba <romadoliba@gmail.com>
2024-03-24 14:47:40 +0200

    Finish Additional_HomeWork_01

--- HomeWork_01/Assets/Scripts/LocationBuilder.cs
+++ HomeWork_01/Assets/Scripts/LocationBuilder.cs
@@ -17,6 +17,10 @@ public class LocationBuilder : MonoBehaviour
         
     }
 
+    public Material enemyMaterial;
+    public Material playerMaterial;
+    public Rigidbody enemyRigidbody;
+    public Rigidbody playerRigidbody;
     private void BuildLocation()
     {
         GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -29,14 +33,24 @@ public class LocationBuilder : MonoBehaviour
         player.transform.localScale = new Vector3 (0.5f, 1, 0.5f);
         player.name = new string ("Player");
         player.AddComponent<Rigidbody>();
+        playerRigidbody = player.GetComponent<Rigidbody>();
+        playerRigidbody.freezeRotation = true;
         player.AddComponent<PlayerMovement>();
         player.AddComponent<Triggered>();
+        playerMaterial = player.GetComponent<Renderer>().material;
+        playerMaterial.color = Color.green;
 
         GameObject enemy = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        enemy.transform.position = new Vector3 (1, 0, 0);
+        enemy.transform.position = new Vector3 (1, 1, 0);
         enemy.transform.localScale = new Vector3 (0.5f, 1, 0.5f);
         enemy.name = new string ("Enemy");
         enemy.AddComponent<Rigidbody>();
+        enemyRigidbody = enemy.GetComponent<Rigidbody>();
+        enemyRigidbody.drag = 10;
+        enemyRigidbody.freezeRotation = true;
+        enemyMaterial = enemy.GetComponent<Renderer>().material;
+        enemyMaterial.color = Color.red;
+
         
         
         
